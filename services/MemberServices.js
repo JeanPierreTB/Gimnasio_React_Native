edd72e8e077bcb9335786c41046731b222ff63b3
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-11-29 22:28:07 -0500

    services de miembro agregados

---
+++ services/MemberServices.js
@@ -0,0 +1,62 @@
+import { BASE_URL } from '../configs/constants';
+import axios from 'axios';
+
+export const fetchBodyParts = (memberId = null) => {
+  const url = BASE_URL + 'member/body_parts' + (memberId != null ? '?member_id=' + memberId : '');
+  // console.log(url);
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        console.error(error.stack);
+        reject(error);
+      });
+  });
+}
+
+export const fetchExercisesBodyPartsCount = (memberId = null) => {
+  const url = BASE_URL + 'member/exercises_body_parts' + (memberId != null ? '?member_id=' + memberId : '');
+  // console.log(url);
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        console.error(error.stack);
+        reject(error);
+      });
+  });
+}
+
+export const fetchExercises = (memberId = null) => {
+  const url = BASE_URL + 'member/exercises' + (memberId != null ? '?member_id=' + memberId : '');
+  // console.log(url);
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        console.error(error.stack);
+        reject(error);
+      });
+  });
+}
+
+export const findExerciseMember = (memberId, exerciseId) => {
+  const url = BASE_URL + 'exercise/find?exercise_id=' + exerciseId + '&member_id=' + memberId;
+  console.log(url);
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        console.error(error.stack);
+        reject(error);
+      });
+  });
+}
